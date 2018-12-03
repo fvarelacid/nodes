da4d82644af0804d14633813cedd58057bfa02e3
tropadicesse <fvarelacid@gmail.com>
2018-12-03 10:06:32 +0000

    Ready to send to Alexandre

---
+++ linked_lists.py
@@ -0,0 +1,89 @@
+class Node:
+  def __init__(self, value, next_node=None):
+    self.value = value
+    self.next_node = next_node
+    
+  def get_value(self):
+    return self.value
+  
+  def get_next_node(self):
+    return self.next_node
+  
+  def set_next_node(self, next_node):
+    self.next_node = next_node
+
+class LinkedList:
+  def __init__(self, value=None):
+    self.head_node = Node(value)
+  
+  def get_head_node(self):
+    return self.head_node
+  
+  def insert_beginning(self, new_value):
+    new_node = Node(new_value)
+    new_node.set_next_node(self.head_node)
+    self.head_node = new_node
+    
+  def stringify_list(self):
+    string_list = ""
+    current_node = self.get_head_node()
+    while current_node:
+      if current_node.get_value() != None:
+        string_list += str(current_node.get_value()) + "\n"
+      current_node = current_node.get_next_node()
+    return string_list
+  
+  def remove_node(self, value_to_remove):
+    current_node = self.get_head_node()
+    if current_node.get_value() == value_to_remove:
+      self.get_head_node = current_node.get_next_node()
+    else:
+      while current_node:
+        next_node = current_node.get_next_node()
+        if next_node == value_to_remove:
+          current_node.set_next_node(next_node.get_next_node())
+          current_node = None
+        else:
+          current_node = next_node
+
+  def remove_every(self, value_to_remove):
+    """This method removes every node that contains the value value_to_remove"""
+    if self.head_node == None:
+    # empty linked list, return
+      return
+    # Remove all consecutive value_to_remove occurrences at the head
+    while self.head_node.value == value_to_remove:
+      # Remove the next node
+      self.head_node = self.head_node.next_node
+      if self.head_node == None:
+      # linked list is now empty
+        return 
+  # Remove all additional occurrences of value_to_remove
+    current_node = self.head_node
+    while current_node.next_node != None:
+      if current_node.next_node.value == value_to_remove:
+      # Remove the next node
+        current_node.next_node = current_node.next_node.next_node
+      else:
+      # Do not remove next node; simply advance to the next node
+        current_node = current_node.next_node
+    # reached end of linked list
+      return
+
+
+
+ll = LinkedList(64)
+ll.insert_beginning(4)
+ll.insert_beginning(34)
+ll.insert_beginning(8)
+ll.insert_beginning(32)
+
+print(ll.stringify_list())
+
+#ll.remove_node(64)
+
+#print(ll.stringify_list())
+
+ll.remove_every(64)
+
+print(ll.stringify_list())
